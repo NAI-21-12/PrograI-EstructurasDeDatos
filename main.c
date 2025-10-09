2d7174d4997ee4a06490e7f6a20b1dc80a7a5663
Rafael <jorabaqu2580@gmail.com>
2025-10-09 12:13:10 -0600

    imprimir sirve

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct país{
     struct país* sigt;
-    int nombre;
-    int problema1;
-    int problema2;
+    char* nombre;
+    char* problema1;
+    char* problema2;
     struct país* ant;
 };
 
@@ -13,7 +14,7 @@ struct lista{
     struct país* final;
 };
 
-struct país* crearPaís(int nombre, int problema1, int problema2){
+struct país* crearPaís(char* nombre, char* problema1, char* problema2){
     struct país* nuevoPaís = calloc(1,sizeof(struct país));
     if (nuevoPaís != NULL){
         nuevoPaís->nombre = nombre;
@@ -25,11 +26,38 @@ struct país* crearPaís(int nombre, int problema1, int problema2){
     return nuevoPaís;
 };
 
+int imprimirPaíses(struct lista* lista){
+    struct país* actual = lista->inicio;
+    while (actual != NULL){
+        printf("País: %s, Problema 1: %s, Problema2: %s\n",actual->nombre,actual->problema1,actual->problema2);
+        actual = actual->sigt;
+    }
+    return 0;
+}
+
+
+int insertarFinal(struct lista* lista, char* nombre, char* problema1, char* problema2){
+    struct país* nuevoPaís = crearPaís(nombre,problema1,problema2);
+    struct país* actual = lista-> inicio;
+
+    if (lista->inicio == NULL){
+        lista->inicio = nuevoPaís;
+        lista->final = nuevoPaís;
+        return 0;
+    }
+
+    lista->final->sigt = nuevoPaís;
+    nuevoPaís->ant = lista->final;
+    lista->final = nuevoPaís;
+    
+    return 0;
+}
 
 
 //Función main que corre el programa
 int main(){
-    printf("Proyecto de Estructuras de Datos\n");
+    struct lista países = {NULL,NULL};
+    insertarFinal(&países,"Costa Rica","Corrupción","Muerte");
+    imprimirPaíses(&países);
     return 0;
 }
-
