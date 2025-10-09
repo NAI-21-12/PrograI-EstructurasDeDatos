86b027f0ab455fe8979275837cd4608fff90fabc
Rafael <jorabaqu2580@gmail.com>
2025-10-09 10:57:59 -0600

    creación de países

--- main.c
+++ main.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
 
+struct país{
+    struct país* sigt;
+    int nombre;
+    int problema1;
+    int problema2;
+    struct país* ant;
+};
+
+struct lista{
+    struct país* inicio;
+    struct país* final;
+};
+
+struct país* crearPaís(int nombre, int problema1, int problema2){
+    struct país* nuevoPaís = calloc(1,sizeof(struct país));
+    if (nuevoPaís != NULL){
+        nuevoPaís->nombre = nombre;
+        nuevoPaís->problema1 = problema1;
+        nuevoPaís->problema2 = problema2;
+        nuevoPaís->sigt = NULL;
+        nuevoPaís->ant = NULL;
+    }
+    return nuevoPaís;
+};
+
+
+
 //Función main que corre el programa
 int main(){
     printf("Proyecto de Estructuras de Datos\n");
     return 0;
 }
+
